551e19b4cb3c88b2256e2716d87db649fd761040
Arex1989 <rexmond.anih@gmail.com>
2026-08-31 21:41:34 +0200

    Manage Azure backup and recovery vaults with Terraform

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -19,3 +19,13 @@ resource "azurerm_data_protection_backup_vault" "main" {
     Purpose     = "CloudEngineeringPortfolio"
   }
 }
+
+resource "azurerm_recovery_services_vault" "main" {
+  name                = "rsv-enterprise-dev-uks"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "RS0"
+
+  storage_mode_type = "GeoRedundant"
+
+}
